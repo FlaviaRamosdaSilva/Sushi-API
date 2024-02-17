d42739a80b4acfaeae9b1cf451adff4a1049a3c1
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-02-16 23:23:25 -0300

    teste-img-product

--- src/App/models/Product.js
+++ src/App/models/Product.js
@@ -11,7 +11,7 @@ class Product extends Model {
       url: {
         type: Sequelize.VIRTUAL, // campo virtual não será gravado
         get () { // mas quando der um get ele vai trazer a url e mostrar a imagem
-          return `https://sushi-api.onrender.com/product-file/${this.path}` // gera pegando esse endereço mais o nome do arquivo path
+          return `https://sushi-api.onrender.com/category-file/${this.path}` // gera pegando esse endereço mais o nome do arquivo path
         }
       }
     },
