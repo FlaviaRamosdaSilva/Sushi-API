db9366256f1b94340f3884a84ac1dd7fbd1c24dd
FlaviaRamosdaSilva <didi.flavia@gmail.com>
2024-02-19 12:12:21 -0300

    ==========

--- src/App/models/Category.js
+++ src/App/models/Category.js
@@ -8,7 +8,7 @@ class Category extends Model {
       url: {
         type: Sequelize.VIRTUAL, // campo virtual não será gravado
         get () { // mas quando der um get ele vai trazer a url e mostrar a imagem
-          return `https://sushi-api.onrender.com/category-file/${this.path}` // gera pegando esse endereço mais o nome do arquivo path
+          return `https://sushi-api.up.railway.app/category-file/${this.path}` // gera pegando esse endereço mais o nome do arquivo path
         }
       }
     },
